1a263cec7f33acf584513e114e5af32ada71bc8a
Johan192004 <johanruma@gmail.com>
2026-02-18 22:52:12 -0500

    feat(api-gateway): add global logging filter for request and response tracing

---
+++ api-gateway/src/main/java/com/stockalert/gateway/filters/LoggingFilter.java
@@ -0,0 +1,38 @@
+package com.stockalert.gateway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("Incoming request: {} {} from {}",
+                request.getMethod(),
+                request.getURI().getPath(),
+                request.getRemoteAddress());
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("Completed: {} {} -> status={}",
+                    request.getMethod(),
+                    request.getURI().getPath(),
+                    response.getStatusCode());
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE - 1;
+    }
+}
